42603e6723c27168f4211fc3ee23bc4233632a7a
Glenn Ford <glenn@glennfu.com>
2009-06-23 18:26:08 -0500

    correctly extended jQuery

--- jquery.labelify.js
+++ jquery.labelify.js
@@ -20,107 +20,109 @@
  *
  * @example jQuery('input').labelify('hasLabel'); // return true if the field has a label
  */
-jQuery.fn.labelify = function(settings) {
-  // if the element has a label, return true when 'hasLabel' is passed as an arg
-  if (typeof settings === 'string' && settings === 'hasLabel') {
-    return jQuery(this).data('hasLabel');
-  }
+(function($){
+	$.fn.labelify = function(settings) {
+	  // if the element has a label, return true when 'hasLabel' is passed as an arg
+	  if (typeof settings === 'string' && settings === 'hasLabel') {
+	    return jQuery(this).data('hasLabel');
+	  }
 
-  var showLabel, hideLabel, labeledClass,
-      lookups, lookup,
-      $labelified_elements,
-			$settings;
+	  var showLabel, hideLabel, labeledClass,
+	      lookups, lookup,
+	      $labelified_elements,
+				$settings;
 
-  $settings = jQuery.extend({
-    text: 'title',
-    labeledClass: ''
-  }, settings);
+	  $settings = $.extend({
+	    text: 'title',
+	    labeledClass: ''
+	  }, settings);
 
-  // Compatibility with version 1.3 and prior (double-ls)
-  if ($settings.labelledClass) { $settings.labeledClass = $settings.labelledClass; }
+	  // Compatibility with version 1.3 and prior (double-ls)
+	  if ($settings.labelledClass) { $settings.labeledClass = $settings.labelledClass; }
 
 
-  lookups = {
-    title: function(input) {
-      return jQuery(input).attr('title');
-    },
-    label: function(input) {
-      return jQuery("label[for=" + input.id +"]").text();
-    }
-  };
+	  lookups = {
+	    title: function(input) {
+	      return $(input).attr('title');
+	    },
+	    label: function(input) {
+	      return $("label[for=" + input.id +"]").text();
+	    }
+	  };
 
-  $labelified_elements = jQuery(this);
+	  $labelified_elements = $(this);
 
-  showLabel = function(el){
-    jQuery(el).addClass($settings.labeledClass).data('hasLabel', true);
-    el.value = jQuery(el).data("label");
-  };
-  hideLabel = function(el){
-    el.value = '';
-    jQuery(el).removeClass($settings.labeledClass).data('hasLabel', false);
-  };
-	labeledClass = function(el){
-		$settings.labeledClass;
-	}
+	  showLabel = function(el){
+	    $(el).addClass($settings.labeledClass).data('hasLabel', true);
+	    el.value = $(el).data("label");
+	  };
+	  hideLabel = function(el){
+	    el.value = '';
+	    $(el).removeClass($settings.labeledClass).data('hasLabel', false);
+	  };
+		labeledClass = function(el){
+			$settings.labeledClass;
+		}
 
-  return jQuery(this).each(function() {
-    var $item = jQuery(this),
-        removeValuesOnExit;
+	  return $(this).each(function() {
+	    var $item = $(this),
+	        removeValuesOnExit;
 
-    if (typeof $settings.text === 'string') {
-      lookup = lookups[$settings.text]; // what if not there?
-    } else {
-      lookup = $settings.text; // what if not a fn?
-    }
+	    if (typeof $settings.text === 'string') {
+	      lookup = lookups[$settings.text]; // what if not there?
+	    } else {
+	      lookup = $settings.text; // what if not a fn?
+	    }
 
-    // bail if lookup isn't a function or if it returns undefined
-    if (typeof lookup !== "function" || !lookup(this)) { return; }
+	    // bail if lookup isn't a function or if it returns undefined
+	    if (typeof lookup !== "function" || !lookup(this)) { return; }
 
-    $item.bind('focus.label',function() {
-      if (this.value === jQuery(this).data("label")) { hideLabel(this); }
-    }).bind('blur.label',function(){
-      if (this.value === '') { showLabel(this); }
-    }).data('label',lookup(this).replace(/\n/g,'')); // strip label's newlines
+	    $item.bind('focus.label',function() {
+	      if (this.value === $(this).data("label")) { hideLabel(this); }
+	    }).bind('blur.label',function(){
+	      if (this.value === '') { showLabel(this); }
+	    }).data('label',lookup(this).replace(/\n/g,'')); // strip label's newlines
     
-    removeValuesOnExit = function() {
-      $labelified_elements.each(function(){
-        if (this.value === jQuery(this).data("label")) { hideLabel(this); }
-      });
-    };
+	    removeValuesOnExit = function() {
+	      $labelified_elements.each(function(){
+	        if (this.value === $(this).data("label")) { hideLabel(this); }
+	      });
+	    };
     
-    $item.parents("form").submit(removeValuesOnExit);
-    jQuery(window).unload(removeValuesOnExit);
+	    $item.parents("form").submit(removeValuesOnExit);
+	    $(window).unload(removeValuesOnExit);
     
-    if (this.value !== '') {
-      // user started typing; don't overwrite his/her text!
-      return;
-    }
+	    if (this.value !== '') {
+	      // user started typing; don't overwrite his/her text!
+	      return;
+	    }
 
-    // set the defaults
-    showLabel(this);
-  });
-};
+	    // set the defaults
+	    showLabel(this);
+	  });
+	};
 
-jQuery.fn.extend({
-	serialize: function() {
-		// Find elements currently showing a labelify label
-		var labelified = [];
-		jQuery('input, textarea').each(function() {
-		  if (jQuery(this).labelify('hasLabel')) {
-		    labelified.push(this);
-		  }
-		});
+	$.fn.extend({
+		serialize: function() {
+			// Find elements currently showing a labelify label
+			var labelified = [];
+			$('input, textarea').each(function() {
+			  if ($(this).labelify('hasLabel')) {
+			    labelified.push(this);
+			  }
+			});
 		
-		// Remove labels
-		jQuery.each(labelified, function() { this.value = ''; });
+			// Remove labels
+			$.each(labelified, function() { this.value = ''; });
 
-		// Correctly serialize the form
-		var data = this.serializeArray();
+			// Correctly serialize the form
+			var data = this.serializeArray();
 		
-		// Re-add labels
-		jQuery.each(labelified, function() { this.value = jQuery(this).data("label"); });
+			// Re-add labels
+			$.each(labelified, function() { this.value = $(this).data("label"); });
 
-		// Send the serialization
-		return jQuery.param(data);
-	}
-});
+			// Send the serialization
+			return $.param(data);
+		}
+	});
+})(jQuery);
