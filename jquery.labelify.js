a50de94945368d29ffbdfecc8d8c5920641cc295
Glenn Ford <glenn@glennfu.com>
2009-06-23 09:52:50 -0500

    renamed '$' to 'jQuery' to avoid conflicts with Prototype

--- jquery.labelify.js
+++ jquery.labelify.js
@@ -3,26 +3,27 @@
  * Stuart Langridge, http://www.kryogenix.org/
  * Released into the public domain
  * Date: 25 June 2008
- * Last update: 08 September 2008
+ * Last update: 23 June 2009
  * @author Stuart Langridge
  * @author Garrett LeSage
- * @version 2.0
+ * @author Glenn Sidney
+ * @version 2.0.1
  */
 
 /**
  * Defaults to taking the in-field label from the field's title attribute
- * @example $("input").labelify();
+ * @example jQuery("input").labelify();
  * @param {object|string} [settings] optional parameters to pass
  * @config {string} [text] "title" to use the field's title attribute (default),
  *                         "label" to use its <label> (for="fieldid" must be specified)
  * @config {string} [labeledClass] class applied to the field when it has label text
  *
- * @example $('input').labelify('hasLabel'); // return true if the field has a label
+ * @example jQuery('input').labelify('hasLabel'); // return true if the field has a label
  */
 jQuery.fn.labelify = function(settings) {
   // if the element has a label, return true when 'hasLabel' is passed as an arg
   if (typeof settings === 'string' && settings === 'hasLabel') {
-    return $(this).data('hasLabel');
+    return jQuery(this).data('hasLabel');
   }
 
   settings = jQuery.extend({
@@ -39,26 +40,26 @@ jQuery.fn.labelify = function(settings) {
 
   lookups = {
     title: function(input) {
-      return $(input).attr('title');
+      return jQuery(input).attr('title');
     },
     label: function(input) {
-      return $("label[for=" + input.id +"]").text();
+      return jQuery("label[for=" + input.id +"]").text();
     }
   };
 
-  $labelified_elements = $(this);
+  $labelified_elements = jQuery(this);
 
   showLabel = function(el){
-    el.value = $(el).data("label");
-    $(el).addClass(settings.labeledClass).data('hasLabel', true);
+    el.value = jQuery(el).data("label");
+    jQuery(el).addClass(settings.labeledClass).data('hasLabel', true);
   };
   hideLabel = function(el){
     el.value = el.defaultValue;
-    $(el).removeClass(settings.labeledClass).data('hasLabel', false);
+    jQuery(el).removeClass(settings.labeledClass).data('hasLabel', false);
   };
 
-  return $(this).each(function() {
-    var $item = $(this),
+  return jQuery(this).each(function() {
+    var $item = jQuery(this),
         removeValuesOnExit;
 
     if (typeof settings.text === 'string') {
@@ -71,19 +72,19 @@ jQuery.fn.labelify = function(settings) {
     if (typeof lookup !== "function" || !lookup(this)) { return; }
 
     $item.bind('focus.label',function() {
-      if (this.value === $(this).data("label")) { hideLabel(this); }
+      if (this.value === jQuery(this).data("label")) { hideLabel(this); }
     }).bind('blur.label',function(){
       if (this.value === this.defaultValue) { showLabel(this); }
     }).data('label',lookup(this).replace(/\n/g,'')); // strip label's newlines
     
     removeValuesOnExit = function() {
       $labelified_elements.each(function(){
-        if (this.value === $(this).data("label")) { hideLabel(this); }
+        if (this.value === jQuery(this).data("label")) { hideLabel(this); }
       });
     };
     
     $item.parents("form").submit(removeValuesOnExit);
-    $(window).unload(removeValuesOnExit);
+    jQuery(window).unload(removeValuesOnExit);
     
     if (this.value !== this.defaultValue) {
       // user started typing; don't overwrite his/her text!
